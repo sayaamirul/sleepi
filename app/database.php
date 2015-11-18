dfc866171bcd29da2c2d0b49e57d5a9f1f034d22
Muhammad Amirul Ihsan <muh.amirul.ihsan@gmail.com>
2015-11-18 10:15:43 +0700

    implementing phpdotenv

--- app/database.php
+++ app/database.php
@@ -5,11 +5,11 @@ use Illuminate\Database\Capsule\Manager as Capsule;
 $capsule = new Capsule;
 
 $capsule->addConnection([
-    'driver'    => 'mysql',
-    'host'      => 'localhost',
-    'database'  => 'dbname',
-    'username'  => 'root',
-    'password'  => '',
+    'driver'    => getenv('DB_DRIVER'),
+    'host'      => getenv('DB_HOST'),
+    'database'  => getenv('DB_NAME'),
+    'username'  => getenv('DB_USER'),
+    'password'  => getenv('DB_PASSWORD'),
     'charset'   => 'utf8',
     'collation' => 'utf8_unicode_ci',
     'prefix'    => '',
--- web/index.php
+++ web/index.php
@@ -1,15 +1,9 @@
 <?php
 
-if (PHP_SAPI == 'cli-server') {
-    // To help the built-in PHP dev server, check if the request was actually for
-    // something which should probably be served as a static file
-    $file = __DIR__ . $_SERVER['REQUEST_URI'];
-    if (is_file($file)) {
-        return false;
-    }
-}
-
 require __DIR__ . '/../vendor/autoload.php';
+
+$dotenv = new \Dotenv\Dotenv(__DIR__.'/../');
+$dotenv->load();
 
 $app = new \Slim\App();
 
