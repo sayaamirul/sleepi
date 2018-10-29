5b8b174663fe116230382830fa637acd787f8e6f
Khoirul Afandi <khoirul.afandi@idntimes.com>
2018-10-29 10:36:09 +0700

    keep app to bootstraps

--- web/index.php
+++ web/index.php
@@ -1,16 +1,5 @@
 <?php
 
-require __DIR__ . '/../vendor/autoload.php';
-
-$dotenv = new \Dotenv\Dotenv(__DIR__.'/../');
-$dotenv->load();
-
-$app = new \Slim\App();
-
-require __DIR__ . '/../app/error.php';
-
-require __DIR__ . '/../app/database.php';
-
-require __DIR__ . '/../app/routes.php';
+require __DIR__ . '/../bootstraps/app.php';
 
 $app->run();
